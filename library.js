79c6ab702e6e919d7c4dfa0f0128ce4b16879e7f
Eldor <eldor@protonmail.com>
2023-08-10 22:02:53 +0300

    use new plugin hook introduced in 3.3.0

--- library.js
+++ library.js
@@ -1,28 +1,19 @@
 'use strict';
 
-const User = require.main.require('./src/user');
-
 const pwned = require('./lib/pwned');
 
 exports.registerCheck = async (data) => {
 	const { userData } = data;
 
-	const isPwned = await pwned.check(userData.password);
-	if (isPwned) {
-		throw new Error('[[pwned-passwords:exposed-password]]');
-	}
+	await pwned.checkAndThrow(userData.password);
 
 	return data;
 };
 
-// Hook User.changePassword directly since no plugin hook exists
-// https://github.com/NodeBB/NodeBB/blob/edd2fc38fc2d6f0ff7f344d11236190a44404f5d/src/user/profile.js#L293
-const originalChangePassword = User.changePassword;
-User.changePassword = async function (uid, data) {
-	const isPwned = await pwned.check(data.newPassword);
-	if (isPwned) {
-		throw new Error('[[pwned-passwords:exposed-password]]');
-	}
+exports.passwordCheck = async (data) => {
+	const { password } = data;
 
-	await originalChangePassword(uid, data);
+	await pwned.checkAndThrow(password);
+
+	return data;
 };
